7d742cad60794a4be77b39643d8b30ae10cf3ebc
Brandon Frostbourne <bfrostbourne@gmail.com>
2020-06-30 07:45:13 -0400

    Added License

---
+++ core/norsetech.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3.8
+
+# This is a Python-based program created by Brandon Frostbourne for
+# a Marymount University capstone project for the summer of 2020.
+# See ../license.rst for MIT license.
